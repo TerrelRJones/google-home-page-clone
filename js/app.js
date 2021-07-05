5e9f75415a22c11f6e53a323296026c6a16a5e6a
Terrel Jones <terrelrjones@gmail.com>
2021-07-05 05:12:53 -0700

    modal finsished, and functional

--- js/app.js
+++ js/app.js
@@ -1,16 +1,70 @@
-API_KEY = 'm4G7b4UBSAQdWyoe2tg7lyv_YXGqJuGMpSSi3MMcUxc';
+const API_KEY = 'm4G7b4UBSAQdWyoe2tg7lyv_YXGqJuGMpSSi3MMcUxc';
 
+function fetchData(){
 fetch(`https://api.unsplash.com/photos/?client_id=${API_KEY}`)
 .then((response) => response.json())
 .then((data) => {
-
+// console.log(data);
 
     data.map((photo) => {
-        let pic = document.querySelector('.test');
+        const bgPhoto = photo.urls.full;
+        const bgName = photo.user.name;
+        const id = photo.id;
+
+        let pic = document.querySelector('.modal__right');
         let fullpic = document.createElement('div');
             pic.appendChild(fullpic);
-            fullpic.innerHTML = `<img src="${photo.urls.full}" alt=""></img>`
-        console.log(photo.urls.full)
+            fullpic.innerHTML = `<div class="photo__card" onclick='chooseBg(${JSON.stringify(photo)})'>
+            <div class="photo__bg">
+              <img src="${bgPhoto}" alt="">
+            </div>
+            <div class="photo__description">
+              <h3>${bgName}</h3>
+            </div>
+          </div>`
+        // console.log(id)
     });
 
 });
+}
+
+// Modal functionality
+const modal = document.querySelector('.modal');
+
+const settingsBtn = document.querySelector('.footer__settings-icon');
+const cancelBtn = document.querySelector('.modal__btn-cancel');
+
+//Open Modal
+function showModal(){
+  settingsBtn.addEventListener('click', () => {
+    modal.classList.add('modal--show')
+  })
+}
+
+// Close Modal
+function closeModal(){
+  cancelBtn.addEventListener('click', () => {
+    modal.classList.remove('modal--show');
+  });
+}
+
+const bodyBg = document.querySelector('.body__bg');
+
+function chooseBg(url){
+  const bgPhoto = url.urls.full;
+  localStorage.setItem('backgroundImage', `${bgPhoto}`);
+}
+
+
+function setBg(){
+  let bgImage = localStorage.getItem('backgroundImage');
+        bodyBg.style.backgroundImage = `"url(${bgImage})"`;
+        console.log(bgImage)
+}
+
+
+
+fetchData();
+showModal();
+closeModal();
+
