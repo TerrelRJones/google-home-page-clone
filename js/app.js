4ff0709de9a916c25b514212454fcbaffdd025b9
Terrel Jones <terrelrjones@gmail.com>
2021-07-05 09:25:14 -0700

    added user modal, shortcut cards, & logo

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ function showModal(){
   })
 }
 
-// Close Modal
+// Close Settings Modal
 function closeModal(){
   cancelBtn.addEventListener('click', () => {
     modal.classList.remove('modal--show');
@@ -55,16 +55,35 @@ function chooseBg(url){
   localStorage.setItem('backgroundImage', `${bgPhoto}`);
 }
 
-
 function setBg(){
   let bgImage = localStorage.getItem('backgroundImage');
         bodyBg.style.backgroundImage = `"url(${bgImage})"`;
         console.log(bgImage)
 }
 
+const userIconNav = document.querySelector('.nav__user-icon');
+const userModal = document.querySelector('.user__modal');
+
+function showUserModal(){
+  userIconNav.addEventListener('click', () => {
+    userModal.classList.add('user__modal--show')
+    console.log('clicked user modal')
+  })
+}
+
+function closeUserModal(){
+  window.addEventListener('mouseup', (e) => {
+      if(e.target != userModal && e.target.parentNode != userModal){
+    userModal.classList.remove('user__modal--show')
+    console.log('body clicked')
+  }
+})
+}
 
 
 fetchData();
+showUserModal();
+closeUserModal();
 showModal();
 closeModal();
 
